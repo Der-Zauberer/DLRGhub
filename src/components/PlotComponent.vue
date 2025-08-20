01bd555980b1b75eb9f05bee741f3983b8263ae1
Der-Zauberer <and.som282@gmail.com>
2025-08-20 18:44:51 +0200

    feat: plot fancyness

--- src/components/PlotComponent.vue
+++ src/components/PlotComponent.vue
@@ -19,8 +19,8 @@ onMounted(async () => {
     const elementColor = getComputedStyle(canvas).getPropertyValue('--theme-element-primary-color').trim()
 
     const padding = 10
-    const width = canvas.width - (padding * 2)
-    const height = canvas.height - (padding * 2)
+    const width = canvas.width - padding
+    const height = canvas.height - padding
     
     const min = Math.min(...props.y)
     const max = Math.max(...props.y)
@@ -28,28 +28,31 @@ onMounted(async () => {
     const stepY = height / (max - min)
 
     const calcX = (x: number) => stepX * x + padding
-    const calcY = (y: number) => height + padding - (y - min) * stepY
+    const calcY = (y: number) => height - (y - min) * stepY
 
     context.beginPath()
-    context.moveTo(padding, padding)
-    context.lineTo(padding, padding + height)
-    context.lineTo(padding + width, padding + height)
+    context.moveTo(padding, 1)
+    context.lineTo(width + padding - 1, 1)
+    context.lineTo(width + padding - 1, height)
+    context.lineTo(padding, height)
+    context.lineTo(padding, 0)
     context.strokeStyle = elementColor
     context.stroke()
 
     context.fillStyle = elementColor
-    const step = max - min > 5 ? Math.floor((max - min) / 5) : 1
-    for (let i = Math.floor(min); i <= Math.floor(max); i += step)  context.fillText(i.toString(), 0, calcY(i))
-    context.fillText(Math.floor(min).toString(), 0, calcY(Math.floor(min)))
+    const step = max - min > 5 ? Math.floor((max - min) / 4) : 1
+    for (let i = Math.floor(min); i <= Math.floor(max); i += step) {
+        if (i !== Math.floor(min) && i !== max) {
+            context.beginPath()
+            context.moveTo(padding, calcY(i))
+            context.lineTo(width + padding - 1,  calcY(i))
+            context.strokeStyle = elementColor
+            context.stroke()
+            context.fillText(i.toString(), 0, calcY(i) + 2)
+        }
 
-    /*
-    props.y.forEach((entry, index) => {
-        context.beginPath()
-        context.arc(calcX(index), calcY(entry), 4, 0, Math.PI*2)
-        context.fillStyle = accentColor
-        context.fill()
-    })
-    */
+    }
+    context.fillText(Math.floor(min).toString(), 0, calcY(Math.floor(min)) + 2)
 
     context.beginPath()
     context.strokeStyle = accentColor
