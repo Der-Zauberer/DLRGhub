182f75a6a6466c746879ef9a1d3297df91d3f063
FabiTheGuy <fabian.doeller@mailfence.com>
2025-08-21 13:10:53 +0200

    feat: add basic touch inspection

--- src/components/PlotComponent2.vue
+++ src/components/PlotComponent2.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2000 1000" style="width: 100%; aspect-ratio: 2/1;" ref="svg" @mousemove="inspect($event)" @mouseleave="pointer = undefined">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2000 1000" style="width: 100%; aspect-ratio: 2/1;" ref="svg" @mousemove="inspectMouse($event)"  @touchstart="" @touchmove="inspectTouch($event)" @mouseleave="pointer = undefined" @touchend="pointer = undefined">
         <path d="M10 10 H1990 V990 H10 Z" stroke-width="10" stroke="#808080" fill="none"/>
         <path :d="`M${values.points[0].x} ${values.points[0].y} ${values.points.map(element => `L${element.x} ${element.y}`).join(' ')}`" stroke-width="10" stroke="yellow" fill="none"/>
         <path v-if="pointer" :d="`M${pointer.x} 10 V999`" stroke-width="10" stroke="white" fill="none"/>
@@ -13,6 +13,7 @@ const props = defineProps<{ x: number[] | string[], y: number[] }>()
 const svg = useTemplateRef('svg')
 
 const pointer = ref<{ x: string, y: string, value: { x: number | string, y: number } } | undefined>(undefined)
+const touchObjects = []
 
 const values = computed(() => {
     const [height, width] = [1000, 2000]
@@ -25,10 +26,19 @@ const values = computed(() => {
     return { height, width, xMin, xMax, yMin, yMax, stepX, stepY, points }
 })
 
-function inspect(event: MouseEvent) {
+function inspectMouse(event: MouseEvent) {
     const rect = svg.value?.getBoundingClientRect()
     if (!rect || !rect.left) return
     const x = Math.round((event.clientX - rect.left) * (values.value.width / rect.width) / values.value.stepX)
+    pointer.value = { x: (x * values.value.stepX).toFixed(0), y: (values.value.height - (props.y[x] - values.value.yMin) * values.value.stepY).toFixed(0), value: { x: props.x[x], y: props.y[x] }}
+}
+
+function inspectTouch(event: TouchEvent) {
+    const rect = svg.value?.getBoundingClientRect()
+    if (!rect || !rect.left) return    
+    const touchPosX = event.touches?.[0]?.clientX;
+    if (!touchPosX) return
+    const x = Math.round((touchPosX - rect.left) * (values.value.width / rect.width) / values.value.stepX)
     pointer.value = { x: (x * values.value.stepX).toFixed(0), y: (values.value.height - (props.y[x] - values.value.yMin) * values.value.stepY).toFixed(0), value: { x: props.x[x], y: props.y[x] }}
 }
 
