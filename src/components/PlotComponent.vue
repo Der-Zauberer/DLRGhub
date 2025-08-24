31867bc07d2df6686ff36243211509369ff88efe
Der-Zauberer <and.som282@gmail.com>
2025-08-24 10:46:20 +0200

    fix: removal of depricated code

--- src/components/PlotComponent.vue
+++ src/components/PlotComponent.vue
@@ -1,72 +1,46 @@
 <template>
-     <canvas ref="canvas" class="width-100"></canvas>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2000 1000" style="width: 100%; aspect-ratio: 2/1;" ref="svg" @mousemove="inspectMouse($event)" @mouseleave="pointer = undefined" @touchmove="inspectTouch($event)" @touchend="pointer = undefined" @touchcancel="pointer = undefined">
+        <path d="M10 10 H1990 V990 H10 Z" stroke-width="10" stroke="#808080" fill="none"/>
+        <path :d="`M${values.points[0].x} ${values.points[0].y} ${values.points.map(element => `L${element.x} ${element.y}`).join(' ')}`" stroke-width="10" stroke="yellow" fill="none"/> 
+        <path v-if="pointer" :d="`M${pointer.x} 10 V999`" stroke-width="10" stroke="white" fill="none"/>
+        <circle v-if="pointer" r="20" :cx="pointer.x" :cy="pointer.y" stroke-width="2" stroke="white" fill="white"/>
+    </svg>
 </template>
 
 <script setup lang="ts">
-import { onMounted, useTemplateRef } from 'vue';
+import { computed, ref, useTemplateRef } from 'vue';
+const props = defineProps<{ x: number[] | string[], y: number[] }>()
+const svg = useTemplateRef('svg')
 
-const props = defineProps<{ x: string[], y: number[] }>()
-const canvasRef = useTemplateRef('canvas')
+const pointer = ref<{ x: string, y: string, value: { x: number | string, y: number } } | undefined>(undefined)
 
-onMounted(async () => {
-
-    const canvas = canvasRef.value
-    if (!canvas) return
-    const context = canvas.getContext('2d')
-    if (!context) return
-
-    const accentColor = getComputedStyle(canvas).getPropertyValue('--theme-accent-color').trim()
-    const elementColor = getComputedStyle(canvas).getPropertyValue('--theme-element-primary-color').trim()
-
-    const padding = 10
-    const width = canvas.width - padding
-    const height = canvas.height - padding
-    
-    const min = Math.min(...props.y)
-    const max = Math.max(...props.y)
-    const stepX = width / (props.x.length - 1)
-    const stepY = height / (max - min)
-
-    const calcX = (x: number) => stepX * x + padding
-    const calcY = (y: number) => height - (y - min) * stepY
-
-    context.beginPath()
-    context.moveTo(padding, 1)
-    context.lineTo(width + padding - 1, 1)
-    context.lineTo(width + padding - 1, height)
-    context.lineTo(padding, height)
-    context.lineTo(padding, 0)
-    context.strokeStyle = elementColor
-    context.stroke()
-
-    context.fillStyle = elementColor
-    const step = max - min > 5 ? Math.floor((max - min) / 4) : 1
-    for (let i = Math.floor(min); i <= Math.floor(max); i += step) {
-        if (i !== Math.floor(min) && i !== max) {
-            context.beginPath()
-            context.moveTo(padding, calcY(i))
-            context.lineTo(width + padding - 1,  calcY(i))
-            context.strokeStyle = elementColor
-            context.stroke()
-            context.fillText(i.toString(), 0, calcY(i) + 2)
-        }
-
-    }
-    context.fillText(Math.floor(min).toString(), 0, calcY(Math.floor(min)) + 2)
-
-    context.beginPath()
-    context.strokeStyle = accentColor
-    props.y.forEach((entry, index) => {
-        const [x, y] = [calcX(index), calcY(entry)]
-        if (index === 0) {
-            context.moveTo(x, y)
-        } else {
-            context.lineTo(x, y)
-        }
-    })
-    context.lineTo(calcX(props.y.length - 1), calcY(props.y[props.y.length - 1]))
-    context.stroke()
-
+const values = computed(() => {
+    const [height, width] = [1000, 2000]
+    //const xValues = props.x.map(entry => typeof entry === 'number' ? entry : new Date(entry).valueOf())
+    const [xMin, xMax] = /*[Math.min(...xValues), Math.max(...xValues)]*/ [0, props.x.length - 1]
+    const [yMin, yMax] = [Math.min(...props.y), Math.max(...props.y)]
+    const [stepX, stepY] = [width / (xMax - xMin), height / (yMax - yMin)]
+    const points = []
+    for (let i = 0; i < props.y.length; i++) points.push({ x: (i * stepX).toFixed(0), y: (height - (props.y[i] - yMin) * stepY).toFixed(0) })
+    return { height, width, xMin, xMax, yMin, yMax, stepX, stepY, points }
 })
+
+function inspectMouse(event: MouseEvent) {
+    const rect = svg.value?.getBoundingClientRect()
+    if (!rect || !rect.left) return
+    const x = Math.round((event.clientX - rect.left) * (values.value.width / rect.width) / values.value.stepX)
+    pointer.value = { x: (x * values.value.stepX).toFixed(0), y: (values.value.height - (props.y[x] - values.value.yMin) * values.value.stepY).toFixed(0), value: { x: props.x[x], y: props.y[x] }}
+}
+
+function inspectTouch(event: TouchEvent) {
+    const rect = svg.value?.getBoundingClientRect()
+    if (!rect || !rect.left) return    
+    const touchPosX = event.touches?.[0]?.clientX;
+    if (!touchPosX) return
+
+    event.preventDefault();
+    const x = Math.round((touchPosX - rect.left) * (values.value.width / rect.width) / values.value.stepX)
+    pointer.value = { x: (x * values.value.stepX).toFixed(0), y: (values.value.height - (props.y[x] - values.value.yMin) * values.value.stepY).toFixed(0), value: { x: props.x[x], y: props.y[x] }}
+}
 
 </script>
