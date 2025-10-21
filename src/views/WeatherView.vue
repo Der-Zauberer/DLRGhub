102b229eaf117518ad907e6bde68f85ebb196b6b
Der-Zauberer <and.som282@gmail.com>
2025-10-21 06:07:56 +0200

    feat: weather preview widget and seven-day-prediction

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -3,11 +3,52 @@
     <div class="container-xxl grid-cols-sm-2 grid-cols-1">
 
       <swd-card>
-        <PlotComponent v-if="weather.value" title="Wetter" subtitle="Gailingen am Hochrhein" :current="weather.value.current.temperature_2m" :x="weather.value.hourly.time" :y="weather.value.hourly.temperature_2m"/>
+        <h4>
+          Wetter
+          <swd-subtitle>Gailingen am Hochrhein</swd-subtitle>
+        </h4>
+
+        <div class="flex flex-wrap flex-space-between">
+
+          <div class="weather-preview">
+            <swd-icon class="weather-icon weather-preview__icon"></swd-icon>
+            <div>
+              <div class="weather-preview__text">{{ weather.value?.current.temperature_2m.toFixed(1) }}°</div>
+              <swd-subtitle>T: {{ (weather.value?.daily.temperature_2m_min[0] || 0).toFixed(1) }}° H: {{ (weather.value?.daily.temperature_2m_max[0] || 0).toFixed(1) }}°</swd-subtitle>
+            </div>
+          </div>
+
+          <div class="weather-preview">
+            <swd-icon class="water-icon weather-preview__icon"></swd-icon>
+            <div class="weather-preview__text">{{ water.value?.temperature[0].toFixed(1) }}°</div>
+          </div>
+
+        </div>
+
       </swd-card>
 
       <swd-card>
-        <PlotComponent v-if="water.value" title="Wassertemperatur" subtitle="Neuhausen" :x="water.value.time" :y="water.value.temperature"/>
+        <h4>
+          7-Tage-Trend
+          <swd-subtitle>Gailingen am Hochrhein</swd-subtitle>
+        </h4>
+        <div class="weather-grid">
+          <div class="contents" v-for="(value, index) in weather.value?.daily.time">
+            <div>{{ new Date(value).toLocaleString([], { weekday: 'short' }).slice(0, 2) }}:</div>
+            <div>{{ (weather.value?.daily.precipitation_probability_mean[index] || 0) + '%' }}</div>
+            <div class="weather-grid__value">{{ (weather.value?.daily.temperature_2m_min[index] || 0).toFixed(1) }}°</div>
+            <div>-</div>
+            <div class="weather-grid__value">{{ (weather.value?.daily.temperature_2m_max[index] || 0).toFixed(1)}}°</div>
+          </div>
+        </div>
+      </swd-card>
+
+      <swd-card>
+        <PlotComponent v-if="weather.value" title="Wetter" subtitle="Gailingen am Hochrhein" :current="weather.value.current.temperature_2m" :x="weather.value.hourly.time.map(mapLocalDate)" :y="weather.value.hourly.temperature_2m"/>
+      </swd-card>
+
+      <swd-card>
+        <PlotComponent v-if="water.value" title="Wassertemperatur" subtitle="Neuhausen" :x="water.value.time.map(mapLocalDate)" :y="water.value.temperature"/>
       </swd-card>
 
       <swd-card>
@@ -17,6 +58,43 @@
     </div>
 
 </template>
+
+<style>
+
+.weather-preview {
+  display: flex;
+  gap: 0.6em;
+}
+
+.weather-preview .weather-preview__icon {
+  font-size: 2.2em;
+}
+
+.weather-preview .weather-preview__text {
+  font-size: 1.4em;
+  align-self: center;
+}
+
+.weather-grid {
+  display: grid;
+  grid-template-columns: fit-content(0) fit-content(0) fit-content(0) fit-content(0) fit-content(0);
+  box-sizing: border-box;
+  white-space: nowrap;
+  gap: 0 0.5em;
+  align-items: center;
+}
+
+.weather-grid div {
+  box-sizing: border-box;
+  height: round(2.2em, 1px);
+  padding: round(0.5em, 1px) 0;
+}
+
+.weather-grid .weather-grid__value {
+  justify-self: end;
+}
+
+</style>
 
 <script setup lang="ts">
 import PlotComponent from '@/components/PlotComponent.vue';
@@ -33,5 +111,9 @@ const weather = resource({
 const water = resource({
   loader: () => weatherService.getWaterTemperature()
 })
+
+function mapLocalDate(date: string): string {
+  return new Date(date).toLocaleString([], { day: '2-digit', month: '2-digit', hour: '2-digit', minute: '2-digit' })
+}
 
 </script>
