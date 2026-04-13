8a391e2fe9c371e42422869fd7cb477914880347
FabiTheGuy <fabian.doeller@mailfence.com>
2026-04-13 16:33:06 +0200

    feat: add temperature color calculation for seven day weather prediction

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -16,7 +16,7 @@
 					<div>{{ prediction.time }}:</div>
 					<div>{{ prediction.rain + '%' }}</div>
 					<div>
-						<div class="weather-prediction-grid__bar" :style="`margin-left: ${prediction.minOffset}%; width: ${100 - prediction.minOffset - prediction.maxOffset}%;`">
+						<div class="weather-prediction-grid__bar" :style="`margin-left: ${prediction.minOffset}%; width: ${100 - prediction.minOffset - prediction.maxOffset}%; background: linear-gradient(90deg, hsl(${prediction.hslMin}, 100%, 50%), hsl(${prediction.hslMax}, 100%, 50%))`">
 							<span>{{ prediction.min }}°</span>
 							<span>{{ prediction.max}}°</span>
 						</div>
@@ -99,11 +99,11 @@ function mapLocalDate(date: string | number): string {
 	return localDate.toLocaleString([], new Date().toDateString() === localDate.toDateString() ? { hour: '2-digit', minute: '2-digit' } : { day: '2-digit', month: '2-digit', hour: '2-digit', minute: '2-digit' })
 }
 
-function getSevenDayPrediction(weather: Weather): { time: string, rain: number, min: number, max: number, minOffset: number, maxOffset: number }[] {
+function getSevenDayPrediction(weather: Weather): { time: string, rain: number, min: number, max: number, minOffset: number, maxOffset: number, hslMin: number, hslMax: number }[] {
 	const allMin = Math.round(Math.min(...weather.daily.temperature_2m_min))
 	const allMax = Math.round(Math.max(...weather.daily.temperature_2m_max))
 	const range = allMax - allMin
-	const prediction : { time: string, rain: number, min: number, max: number, minOffset: number, maxOffset: number }[] = []
+	const prediction : { time: string, rain: number, min: number, max: number, minOffset: number, maxOffset: number, hslMin: number, hslMax: number }[] = []
 	for (const [index, time] of weather.daily.time.entries()) {
 		const min = Math.round(weather.daily.temperature_2m_min[index])
 		const max = Math.round(weather.daily.temperature_2m_max[index])
@@ -114,9 +114,28 @@ function getSevenDayPrediction(weather: Weather): { time: string, rain: number, 
 			max,
 			minOffset: (min - allMin) / range * 100,
 			maxOffset: (allMax - max) / range * 100,
+			hslMin: calculateTemperatureHslValue(min),
+			hslMax: calculateTemperatureHslValue(max),
 		})
 	}
 	return prediction
+}
+
+function calculateTemperatureHslValue(temperature: number): number {
+	const ppoints: { temp: number, hsl: number }[] = [ { temp: 0, hsl: 230 }, { temp: 20, hsl: 125 }, { temp: 35, hsl: 30 }, { temp: 40, hsl: 310 } ]
+	ppoints.sort((x: { temp: number, hsl: number }, y: { temp: number, hsl: number }) => x.temp - y.temp)
+	if (temperature <= ppoints[0].temp) return ppoints[0].hsl
+	if (temperature >= ppoints[ppoints.length - 1].temp) return ppoints[ppoints.length - 1].hsl
+	for (let i = 0; i < ppoints.length - 1; i++) {
+		if (temperature >= ppoints[i].temp && temperature <= ppoints[i + 1].temp) {
+			const alpha = (temperature - ppoints[i].temp) / (ppoints[i + 1].temp - ppoints[i].temp)
+			const delta = ((ppoints[i + 1].hsl - ppoints[i].hsl + 540) % 360) - 180;
+			return (((ppoints[i].hsl + alpha * delta) % 360) + 360) % 360
+		}
+	}
+
+	// Should never end here, just here for compile to shut up
+	return ppoints[ppoints.length - 1].hsl
 }
 
 </script>
